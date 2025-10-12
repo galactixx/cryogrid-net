77ddd7b5ac26b62c9978eba452bb1036643f9c8f
galactixx <nwlezien.github@gmail.com>
2025-10-12 18:11:25 -0500

    update seed_everything

--- utils.py
+++ utils.py
@@ -95,3 +95,8 @@ def seed_everything(seed: int) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cuda.matmul.allow_tf32 = False
+    torch.backends.cudnn.allow_tf32 = False
